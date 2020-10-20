c328fb909d75fbb8bd5736b989e56da896ecfccc
Rodrigo Berasain <rodriberasain@gmail.com>
2020-10-20 15:39:51 -0300

    Dev: Adding Allure report

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -9,8 +9,8 @@ import org.testng.annotations.Test;
 
 public class SearchTest extends BaseTestWeb {
 		
-	@Test (priority = 0, description = "Search an item in aliexpress")
-	@Description("Search an item in aliexpress")
+	@Test (priority = 0, description = "Check second product of second result page availability")
+	@Description("As a Customer we want to see if the second ad from the second results page when searching for \"Iphone\" on www.aliexpress.com has at least 1 item to be bought.")
 	private void search() throws Throwable {
 		ResultsPage resultsPage = homePage.searchItem("iPhone");
 		ItemDetailsPage itemDetailsPage =
